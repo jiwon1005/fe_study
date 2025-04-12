1d5fbcca7a5fbace4875f7c1216d59874c64dd33
sjiwon1005 <145963645+sjiwon1005@users.noreply.github.com>
2025-04-12 17:37:13 +0900

    입력값 없을 시 알럿 노출

--- 1_todo_app/todo.js
+++ 1_todo_app/todo.js
@@ -2,6 +2,11 @@ function createTodo() {
     let todoInput = document.getElementById ('todo-input')
     let inputText = todoInput.value
 
+    if (inputText == '') {
+        alert('할 일을 입력해 주세요!')
+        return false
+    }
+
     let todoList = document.getElementById('todo-list')
     let newTodo = document.createElement('li')
     newTodo.innerText = inputText
